fbab2492f0004382c7ab0d4e7a19220f178326e8
ayush-code-drops <ayushmangal121212@gmail.com>
2021-10-27 20:21:10 +0530

    intial commit

---
+++ components/navbar.js
@@ -0,0 +1,34 @@
+ function navbar() {
+    return `<div id="navbar-logo">
+    <img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5fc4feea074f5b86a6c62285_freshly-logo-R.svg" alt="" class="logo-img">
+</div>
+
+<ul id="navbar-menu">
+   <li class="nav-list-items">
+       <a href="" class="nav-links">Plans & Menu</a>
+   </li> 
+   <li class="nav-list-items">
+    <a href="" class="nav-links">How It Works</a>
+</li> 
+<li class="nav-list-items">
+    <a href="" class="nav-links">Gifts</a>
+</li> 
+<li class="nav-list-items">
+    <a href="" class="nav-links">Refer a Friend</a>
+</li> 
+<li class="nav-list-items">
+    <a href="" class="nav-links">Help</a>
+</li> 
+   
+
+
+</ul>
+<div id="navbar-sec3">
+    <a href="" class="nav-links">Login</a>
+    <button class="btn btn-primary btn-md">Sign Up</button>
+</div>
+`
+}
+
+export default navbar
+
---
+++ script.js
@@ -0,0 +1,5 @@
+import navbar from "./components/navbar.js";
+
+const navbarElement = document.getElementById('navbar');
+navbarElement.innerHTML = navbar()
+
