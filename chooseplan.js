0b1fac1356ba7fe540cae9b9bf733f54c199eec4
prajwal799 <prajwalgoyal1999@gmail.com>
2021-10-27 21:23:28 +0530

    sign.html and chhoseplan.html files

---
+++ chooseplan.js
@@ -0,0 +1,10 @@
+import Navbar from "./components/nav1.js";
+
+
+const navbar = Navbar();
+const container = document.getElementById("nav");
+container.innerHTML = navbar;
+
+
+
+
---
+++ components/nav1.js
@@ -0,0 +1,21 @@
+export function Navbar(){
+    return ` <div>
+    <h3>Freshly</h3>
+</div>
+<h1 class="nav_choose_plan">Choose A Plan</h1>
+<div class="nav_detalis" >
+    <h3>Plan</h3>
+    <i class="fa fa-angle-right"  ></i>
+    <h3>Day</h3>
+    <i class="fa fa-angle-right"></i>
+    <h3>Meals</h3>
+    <i class="fa fa-angle-right" ></i>
+    <h3>Checkout</h3>
+</div>
+ <div>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCXSELx1DeRAjLzqxb9ar1-z9e8yZDjkH7Hg&usqp=CAU" class="question_mark"/>
+     <h2 class="customer_support"><a href="#">Customer support</a></h2>
+ </div>`
+}
+
+export default Navbar;
