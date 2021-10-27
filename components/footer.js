b62f14371589d76c701653dd797b2fa5498eb1a3
prajwal799 <prajwalgoyal1999@gmail.com>
2021-10-27 21:17:46 +0530

    part3 part 4 and footer

---
+++ components/footer.js
@@ -0,0 +1,55 @@
+export function Footer(){
+    return`<div id="footer">
+    <div class="first_part_footer footer_part">
+        <ul>
+            <li><a href="#"><img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5fc4feea074f5b86a6c62285_freshly-logo-R.svg" class="img_freshly_footer"/></a></li>
+            <li ><a href="freshly.html">@ Freshly inc</a></li>
+        </ul>
+    </div>
+    <div class="second_part_footer footer_part">
+        <ul>
+            <li><a href="#" class="company_footer">Company</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Contant Us</a></li>
+            <li><a href="#">Press</a></li>
+            <li><a href="#">Terms</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Affilicat</a></li>
+            <li><a href="#">Partnerships</a></li>
+        </ul>
+    </div>
+    <div class="third_part_footer footer_part">
+        <ul>
+            <li><a href="#" class="company_footer">Learn More</a></li>
+            <li><a href="#">Plans & Menu</a></li>
+            <li><a href="#">Why Freshly?</a></li>
+            <li><a href="#">Gifts</a></li>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Student</a></li>
+        </ul>
+    </div>
+    <div class="four_part_footer footer_part">
+        <ul>
+            <li><a href="#" class="company_footer">Join Us</a></li>
+            <li>
+                <a href="#">
+                   <img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423ad92101de48f410c7_5d10dfc0ef515f74b875a42c_icons8-instagram-50.jpg" class="social_media"/>
+                    <img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423bbc67c09879c5a19b_5d795c37fedbb6017d824b02_icons8-facebook-old-30.jpg" class="social_media"/>
+                    <img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423bd050163202b341e8_5d10dfc00442bfc99d3d6828_icons8-twitter-filled-50.jpg" class="social_media"/>
+                    <img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423a6b26e039d505425e_5d10dfbfef515faad975a42b_icons8-pinterest-filled-50.jpg" class="social_media"/>
+                    </a>
+            </li>
+            <li>
+                <img src="https://w7.pngwing.com/pngs/822/688/png-transparent-apple-on-the-app-store-screenshot-iphone-app-store-google-play-store-electronics-text-logo.png" class="playstore_img"/>
+            </li>
+            <li>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png" class="playstore_img"/>
+            </li>
+            
+        </ul>
+    </div>
+</div>`
+}
+
+export default Footer;
