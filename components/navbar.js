6faa412e38478dd424db88a6c6112cf8b6a65836
skant-2000 <qwertyskant2000@gmail.com>
2021-10-28 18:26:44 +0530

    done some tweaks

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,9 @@
 function navbar() {
     return `<div id="navbar-logo">
+    <a href = "index.html">
     <img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5fc4feea074f5b86a6c62285_freshly-logo-R.svg" alt="" class="logo-img">
+    </a>
+    
 </div>
 
 <ul id="navbar-menu">
