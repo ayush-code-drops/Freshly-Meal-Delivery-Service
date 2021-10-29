1e5a251a80b3edfc1274c767feb27d81bd077b15
prajwal799 <prajwalgoyal1999@gmail.com>
2021-10-29 23:44:59 +0530

    Gift page is added

---
+++ gift.js
@@ -0,0 +1,16 @@
+import Navbar from"./components/navbar.js"
+import footer from"./components/footer.js"
+
+const navbarElement = document.getElementById('navbar');
+navbarElement.innerHTML = Navbar();
+
+const footerElement = document.getElementById('footer');
+footerElement.innerHTML = footer();
+
+const accordion =document.getElementsByClassName('contentBx');
+
+for(let i=0;i<accordion.length;i++){
+    accordion[i].addEventListener('click',function(){
+        this.classList.toggle('active');
+    })
+}
