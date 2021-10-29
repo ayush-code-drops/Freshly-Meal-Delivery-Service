6d167c8c0f11a897d1983ffd4892442a67bc1ad2
ayush-code-drops <ayushmangal121212@gmail.com>
2021-10-29 20:55:44 +0530

    meal section

---
+++ Javascript/meal.js
@@ -0,0 +1,6 @@
+const apiId='e53218a1'
+const url = `https://api.edaman.com`
+const apiKey='cb54f057f9c60bc7242066bdba46fbf3	'
+function getFoodItems() {
+return fetch()
+}
--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,7 @@
  function navbar() {
     return `<div id="navbar-logo">
-    <img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5fc4feea074f5b86a6c62285_freshly-logo-R.svg" alt="" class="logo-img">
+    <a href="./index.html"><img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5fc4feea074f5b86a6c62285_freshly-logo-R.svg" alt="" class="logo-img">
+    </a>
 </div>
 
 <ul id="navbar-menu">
