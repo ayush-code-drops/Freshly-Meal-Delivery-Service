6633c41c79826950050bbb22dccc24f34790198f
prajwal799 <prajwalgoyal1999@gmail.com>
2021-11-01 23:49:59 +0530

    add media query of front page

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ const container = document.getElementById('footer');
 container.innerHTML = footers;
 
 
+const hamber = document.getElementById("hambers");
+hamber.addEventListener("click",ham);
+
+function ham(){
+    document.getElementById("navbar").classList.toggle("active");
+    
+}
+
+var across = document.getElementById("across");
+across.addEventListener("click",acrossfn);
+
+function acrossfn(){
+    location.replace('index.html');
+}
