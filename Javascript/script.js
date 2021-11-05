8c550db29cf48843d4c422e215c94d0486ba67f3
ayush-code-drops <ayushmangal121212@gmail.com>
2021-11-05 13:42:34 +0530

    navbar fix

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,8 +1,10 @@
-import navbar from "./components/navbar.js";
-import Footer from "./components/footer.js"
+import { navbar } from "../components/navbar.js";
+// import Footer from "./components/footer.js"
+
 
 const navbarElement = document.getElementById('navbar');
-navbarElement.innerHTML = navbar()
+
+navbarElement.innerHTML=navbar()
 
 const footers = Footer();
 const container = document.getElementById('footer');
