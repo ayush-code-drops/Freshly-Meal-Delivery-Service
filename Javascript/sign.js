bb6ce26524e8ced164a5b72521eea6519b375f7c
skant-2000 <qwertyskant2000@gmail.com>
2021-11-06 12:58:23 +0530

    push

--- Javascript/sign.js
+++ Javascript/sign.js
@@ -1,8 +1,8 @@
-// import Navbar from "./components/nav1.js";
+import Navbar from "./components/nav1.js";
 
-// const navbar = Navbar();
-// const container = document.getElementById("nav");
-// container.innerHTML = navbar;
+const navbar = Navbar();
+const container = document.getElementById("nav");
+container.innerHTML = navbar;
 
 
 // Sending Mail and Zip to Local Host
